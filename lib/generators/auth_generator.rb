41beaf49c5190c8709008bc2ee9fe2091d86e1b6
fukajun <fukajun.shark@gmail.com>
2012-05-21 20:50:59 +0900

    application controllerに追加するコードを追加

--- lib/generators/auth_generator.rb
+++ lib/generators/auth_generator.rb
@@ -1,23 +1,27 @@
 class AuthGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
+
+  APP_CONTROLLER_LINE =<<-EOS
+  helper_method :current_user
+
+  def current_user
+    begin
+      @current_user || User.find(session[:user_id]) if session[:user_id]
+    rescue
+      nil
+    end
+  end
+
+  def authenticate
+    redirect_to :root unless current_user
+  end
+  EOS
+
   def install
-    sessions_controller_code =<<-EOS
-helper_method :current_user
+    # session_controllerに追加
+    copy_file "sesssions_controller.rb", "app/controllers/sessions_controller.rb"
+    # application controllerに追加
+    inject_into_class "app/controllers/sessions_controller.rb", ApplicationController, APP_CONTROLLER_LINE
 
-def current_user
-  begin
-    @current_user || User.find(session[:user_id]) if session[:user_id]
-  rescue
-    nil 
   end
-end 
-
-def authenticate
-  redirect_to :root unless current_user
-end
-    EOS
-    create_file "controller/sesssions_controller.rb", sessions_controller_code
-    copy_file "sesssions_controller.rb", "app/controller/sessions_controller.rb"
-  end
-
 end
